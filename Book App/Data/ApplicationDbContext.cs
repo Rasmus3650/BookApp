2237bb8e6e81dd631079cbcfa9949afab2a88975
Rasmus Østergaard <au680592@uni.au.dk>
2023-12-03 16:05:12 +0100

    First commit

---
+++ Book App/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Book_App.Models;
+
+namespace Book_App.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Book_App.Models.Book> Book { get; set; } = default!;
+    }
+}
